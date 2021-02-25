0ef61bda4eb3d3d3e1d10663d94f37c7f4aa8a11
Hackerry <j4niu@ucsd.edu>
2021-02-25 10:10:01 -0800

    Finish lab 8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'ZaVd6wqPAPnKyG9NSVvzwzi2qhxmEIe6';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false,
+	});
+
+	L.marker([32.88, -117.236]).addTo(map);
 }
